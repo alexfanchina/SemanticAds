828eeb977e5799eb69b0f088a871816b63e3a34e
alexfanchina <alexfanchina@gmail.com>
2019-04-29 11:15:14 -0700

    fix (LogoDetector)

--- src/test_logo_detector.py
+++ src/test_logo_detector.py
@@ -11,14 +11,14 @@ dataset_idx = 1
 logo_name = 'mcdonalds'
 dataset = DATASETS[dataset_idx]
 logo_frame = dataset['brand_frames'][logo_name]
-logo_path = BRANDS[logo_name]
+# logo_path = BRANDS[logo_name]
 
 # get image
 video_io = VideoIO(dataset['video'], dataset['width'], dataset['height'])
-pil_image = video_io.seek_frame(logo_frame).convert('RGB')
+pil_image = video_io.read_frame(logo_frame).convert('RGB')
 
 # detect
-logo_detector = LogoDetector(pil_image)
-logo_detector.detect_sift(logo_path)
+logo_detector = LogoDetector(BRANDS)
+logo_detector.detect(pil_image)
 
 
