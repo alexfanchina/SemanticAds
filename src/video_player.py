2e61012e82f1515d3fee8140644c5f7178f88a4c
alexfanchina <alexfanchina@gmail.com>
2019-05-01 11:45:22 -0700

    feat (VideoPlayer): add skip forward/backward
    Co-authored-by:  Xinrui Li lixinrui_1995@163.com

--- src/video_player.py
+++ src/video_player.py
@@ -9,20 +9,23 @@ from video_io import VideoIO
 import cv2
 import numpy as np
 import pickle
+from logger import logger
+
 
 class Player:
     def __init__(self, audio_path, video_path, point_path, width, height, fps):
         self.logo_index = 0
         if os.path.exists(point_path):
-            self.logo_array = pickle.load(open(point_path,"rb"))
+            self.logo_array = pickle.load(open(point_path, "rb"))
         else:
             self.logo_array = []
-        self.f = wave.open(audio_path,"rb")
+        self.f = wave.open(audio_path, "rb")
         f = self.f
         self.chunk = int(f.getframerate()/fps)
-        self.p = pyaudio.PyAudio() 
+        self.p = pyaudio.PyAudio()
         p = self.p
-        self.stream = p.open(format = p.get_format_from_width(f.getsampwidth()),channels = f.getnchannels(),rate = f.getframerate(), output = True)  
+        self.stream = p.open(format=p.get_format_from_width(
+            f.getsampwidth()), channels=f.getnchannels(), rate=f.getframerate(), output=True)
         self.data = f.readframes(self.chunk)
         self.audioPath = audio_path
         self.videoPath = video_path
@@ -38,74 +41,99 @@ class Player:
         self.old = -1
         self.pilImage = self.video_io.read_frame(self.index)
         self.tkImage = ImageTk.PhotoImage(image=self.pilImage)
-        self.label2 = Label(self.root, image = self.tkImage)
+        self.label2 = Label(self.root, image=self.tkImage)
         self.label2.pack()
-        self.button_play = Button(self.root,text = 'play',command=self.start)
-        self.button_pause = Button(self.root,text = 'pause',command=self.pause)
-        self.button_stop = Button(self.root,text = 'stop',command=self.stop)
+        self.button_fast_backword = Button(
+            self.root, text='<<', command=self.fast_backword)
+        self.button_play = Button(self.root, text='PLAY', command=self.start)
+        self.button_pause = Button(self.root, text='PAUSE', command=self.pause)
+        self.button_fast_forword = Button(
+            self.root, text='>>', command=self.fast_forword)
+        self.button_stop = Button(self.root, text='STOP', command=self.stop)
+
+        self.button_fast_backword.pack(side=LEFT)
         self.button_play.pack(side=LEFT)
         self.button_pause.pack(side=LEFT)
+        self.button_fast_forword.pack(side=LEFT)
         self.button_stop.pack(side=LEFT)
         self.label1 = Label(self.root, text="")
         self.label1.pack(side=RIGHT)
         self.root.mainloop()
 
+    def fast_forword(self):
+        self.index = self.index+150  # 5s
+        self.old = self.index-1
+        if self.index >= self.frameNum:
+            self.index = self.frameNum-1
+        while len(self.logo_array) > self.logo_index and self.index > self.logo_array[self.logo_index][0]:
+            self.logo_index = self.logo_index+1
 
+    def fast_backword(self):
+        self.index = self.index-150  # 5s
+        self.old = self.index-1
+        if self.index < 0:
+            self.index = 0
+        while self.logo_index > 0 and self.index <= self.logo_array[self.logo_index-1][0]:
+            self.logo_index = self.logo_index-1
 
     def read_wav(self):
-        self.f = wave.open(self.audioPath,"rb") 
-        f = self.f 
-        self.p = pyaudio.PyAudio()  
+        self.f = wave.open(self.audioPath, "rb")
+        f = self.f
+        self.p = pyaudio.PyAudio()
         p = self.p
-        self.stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-                                    channels = f.getnchannels(),
-                                    rate = f.getframerate(), 
-                                    output = True)  
-               
+        self.stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                             channels=f.getnchannels(),
+                             rate=f.getframerate(),
+                             output=True)
+
     def videoplayer(self):
+        if(self.index >= self.frameNum):
+            self.stop()
         if self.state:
             self.root.after(1, self.videoplayer)
-        if self.state and self.index>self.old:
-            if(self.index >= self.frameNum):
-                self.stop()
-            else:
-                self.old = self.index
-                self.pilImage = self.video_io.read_frame(self.index)
-                self.label1.configure(text="")
-                if(len(self.logo_array)>self.logo_index and (self.index-self.logo_array[self.logo_index][0])<2 and (self.index-self.logo_array[self.logo_index][0])>-1):#(index,logo, points)
-                    points = np.array(self.logo_array[self.logo_index][2])
-                    self.pilImage = cv2.cvtColor(np.asarray(self.pilImage), cv2.COLOR_RGB2BGR) 
-                    cv2.polylines(self.pilImage, [points], 1, (0, 0, 255), 5)
-                    self.pilImage = Image.fromarray(cv2.cvtColor(self.pilImage,cv2.COLOR_BGR2RGB))
-                    self.logo_index = self.logo_index+1
-                    self.label1.configure(text=self.logo_array[self.logo_index][1])
-                self.tkImage = ImageTk.PhotoImage(image=self.pilImage)
-                self.label2.configure(imag=self.tkImage)
+        if self.state and self.index > self.old:
+            self.old = self.index
+            self.pilImage = self.video_io.read_frame(self.index)
+            self.label1.configure(text="")
+            # (index,logo, points)
+            if(len(self.logo_array) > self.logo_index and (self.index-self.logo_array[self.logo_index][0]) < 2 and (self.index-self.logo_array[self.logo_index][0]) > -1):
+                points = np.array(self.logo_array[self.logo_index][2])
+                self.pilImage = cv2.cvtColor(
+                    np.asarray(self.pilImage), cv2.COLOR_RGB2BGR)
+                cv2.polylines(self.pilImage, [points], 1, (0, 0, 255), 5)
+                self.pilImage = Image.fromarray(
+                    cv2.cvtColor(self.pilImage, cv2.COLOR_BGR2RGB))
+                self.logo_index = self.logo_index+1
+                self.label1.configure(text=self.logo_array[self.logo_index][1])
+            self.tkImage = ImageTk.PhotoImage(image=self.pilImage)
+            self.label2.configure(imag=self.tkImage)
 
     def audioplayer(self):
-        while (self.data and self.state):
-            self.data = self.f.readframes(self.chunk)  
-            self.index = self.index+1
-            self.stream.write(self.data)  
-        if not self.data:
-            self.stop() 
+        while self.state:
+            if int(self.index*self.f.getframerate()/self.fps) > self.f.getnframes():
+                self.stop()
+            else:
+                self.f.setpos(int(self.index*self.f.getframerate()/self.fps))
+                self.data = self.f.readframes(self.chunk)
+                self.index = self.index+1
+                self.stream.write(self.data)
 
     def start(self):
-        print("start")
-        if self.state==0:           
+        logger.d("start")
+        if self.state == 0:
             self.state = 1
             try:
-                _thread.start_new_thread( self.videoplayer, () )
-                _thread.start_new_thread( self.audioplayer, () )
+                _thread.start_new_thread(self.videoplayer, ())
+                _thread.start_new_thread(self.audioplayer, ())
             except:
-                print ("Error: unable to start thread")
+                logger.e("Error: unable to start thread")
 
     def pause(self):
-        print("pause")
+        logger.d("pause")
         self.state = 0
 
     def stop(self):
-        print("stop")
+        logger.d("stop")
         self.state = 0
         self.old = -1
         self.index = 0
@@ -115,17 +143,11 @@ class Player:
         self.label2.configure(imag=self.tkImage)
         self.root.update_idletasks()
         self.read_wav()
-         
 
 if __name__ == "__main__":
-
+    logger.set_level('i')
     width = 480
-    height =270
+    height = 270
     fps = 30
     point_path = str.split(sys.argv[1], '.rgb')[0]+'.pkl'
-    print(point_path)
     player = Player(sys.argv[2], sys.argv[1], point_path, width, height, fps)
-
-
-
-
